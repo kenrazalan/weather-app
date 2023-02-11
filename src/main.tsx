d4925c568c77bb98fbde69c559b6cc74b8e354ca
kenrazalan <kennethrazalan29@gmail.com>
2023-02-11 18:20:37 +0800

    feat: setup mantine UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { Auth0Provider } from "@auth0/auth0-react";
+import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -10,16 +11,18 @@ const clientId = import.meta.env.VITE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Auth0Provider>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Auth0Provider>
+    </MantineProvider>
   </React.StrictMode>
 );
