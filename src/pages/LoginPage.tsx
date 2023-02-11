d4f1f88081d92cc5a32bb2898b887f8b28963e56
kenrazalan <kennethrazalan29@gmail.com>
2023-02-11 19:44:46 +0800

    feat: Login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,17 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button, Text } from "@mantine/core";
 
 const LoginPage = () => {
   const { loginWithRedirect, user, isAuthenticated } = useAuth0();
 
   return (
     <div>
-      <button onClick={() => loginWithRedirect()}>Log In</button>
+      <Text fz="sm">Welcome to the weather forecast web application.</Text>
+      <Text fz="sm" mb="md">
+        Please login with your Github user to use the application and view the
+        weather in your city.
+      </Text>
+      <Button onClick={() => loginWithRedirect()}>Log In</Button>
     </div>
   );
 };
