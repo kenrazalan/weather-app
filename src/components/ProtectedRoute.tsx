52a159d5846c0ec5d475023d905067ac54c0d5ef
kenrazalan <kennethrazalan29@gmail.com>
2023-02-11 23:55:06 +0800

    feat: add weather page

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({ isAuthenticated }: any) => {
+const ProtectedRoute = ({ isAuthenticated }: { isAuthenticated: boolean }) => {
   return isAuthenticated ? <Outlet /> : <Navigate to="/app/login" />;
 };
 
--- src/pages/WeatherPage.tsx
+++ src/pages/WeatherPage.tsx
@@ -1,9 +1,65 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { Box, Button, SimpleGrid, Text } from "@mantine/core";
+import { useLocation, useNavigate } from "react-router-dom";
+import CardComponent from "../components/Card";
+import { capitalizeFirstLetter } from "../utils/capitatilzeFirsLetter";
 
 export const WeatherPage = () => {
-  const { user, logout } = useAuth0();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { weather, main, name, sys } = location.state;
 
-  return <div className="mb-5">Weather Page</div>;
+  const date = Date.now();
+  const temperatue = main.temp;
+  const description = capitalizeFirstLetter(weather[0].description);
+  const weatherMain = capitalizeFirstLetter(weather[0].main);
+  const pressure = main.pressure;
+  const humidity = main.humidity;
+  const country = sys.country;
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <Box
+      sx={() => ({
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        gap: "5px",
+      })}
+    >
+      <Text size="xl" weight={700} mb="xl">
+        {name}, {country}
+      </Text>
+      <SimpleGrid
+        w={"100%"}
+        cols={6}
+        breakpoints={[
+          { maxWidth: 980, cols: 3, spacing: "md" },
+          { maxWidth: 755, cols: 2, spacing: "sm" },
+          { maxWidth: 600, cols: 1, spacing: "sm" },
+        ]}
+      >
+        <CardComponent title={"Date"} isDate={true} date={date} />
+        <CardComponent title={"Temp(F)"} content={temperatue} />
+        <CardComponent title={"Description"} content={description} />
+        <CardComponent title={"Main"} content={weatherMain} />
+        <CardComponent title={"Pressure"} content={pressure} />
+        <CardComponent title={"Humidity"} content={humidity} />
+      </SimpleGrid>
+      <Button
+        onClick={goBack}
+        mt="lg"
+        sx={() => ({
+          alignSelf: "flex-end",
+        })}
+      >
+        Back
+      </Button>
+    </Box>
+  );
 };
 
 export default WeatherPage;
